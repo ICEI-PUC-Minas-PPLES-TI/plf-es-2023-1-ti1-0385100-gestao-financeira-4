d419ba3660d567951e4a3d0e90434b34a18afc6e
erica-as <erica.asnt@hotmail.com>
2023-05-09 19:11:11 -0300

    ajustes tela grupo

--- codigo/assets/scripts/grupos.js
+++ codigo/assets/scripts/grupos.js
@@ -28,16 +28,3 @@ function buscaGruposAPI() {
 }
 
 window.addEventListener("load", buscaGruposAPI);
-
-{/* <div class="col-md-3">
-    <div class="card">
-    <div class="card-body">
-      <img src="${grupo.image}" class="card-img-top" alt="...">
-        <h5 class="card-title">${grupo.titulo}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <h6 class="card-subtitle mb-2 text-body-secondary">${grupo.evento.quantidade} Eventos</h6>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>
-  </div> */}
