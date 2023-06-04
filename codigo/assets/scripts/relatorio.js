df6ed75b0c12b26f042468b76a0ac1d86e99b929
oarthurfc <arthuryouface@gmail.com>
2023-06-04 11:14:21 -0300

    Calculadora de gastos por usuário implementada

--- codigo/assets/scripts/relatorio.js
+++ codigo/assets/scripts/relatorio.js
@@ -16,73 +16,66 @@ fetch('../assets/data/lancamentos.json')
   });
 */
 
-
 var Lancamentos = [
   {
-    "Descricao": "Rolê",
-    "Categoria": "bebida",
-    "Valor": 30.00,
-    "Data": "12/05/2023",
-    "Grupo": "amigos",
-    "Responsaveis": [
-      "renan", 
-      "julio",
-      "maria"]
+    Descricao: "Rolê",
+    Categoria: "bebida",
+    Valor: 30.0,
+    Data: "12/05/2023",
+    Grupo: "amigos",
+    Responsaveis: ["Renan", "Julio", "Maria"],
   },
   {
-    "Descricao": "Balada",
-    "Categoria": "role",
-    "Valor": 100.00,
-    "Data": "12/05/2023",
-    "Grupo": "amigos",
-    "Responsaveis": [
-      "renan",
-      "julio"
-    ]
+    Descricao: "Balada",
+    Categoria: "role",
+    Valor: 100.0,
+    Data: "12/05/2023",
+    Grupo: "amigos",
+    Responsaveis: ["Renan", "Julio"],
   },
   {
-    "Descricao": "Futebol",
-    "Categoria": "lazer",
-    "Valor": 20.00,
-    "Data": "12/05/2023",
-    "Grupo": "amigos",
-    "Responsaveis": [
-      "renan"
-    ]
-  }
-]
+    Descricao: "Futebol",
+    Categoria: "lazer",
+    Valor: 20.0,
+    Data: "12/05/2023",
+    Grupo: "amigos",
+    Responsaveis: ["Renan"],
+  },
+];
 
-function ExibirLancamentos () {
-  var textoHTML = '';
-  var textoHTML2 = '';
+function ExibirLancamentos() {
+
+  var totalHTML = "";
+
+  // Objeto para armazenar as somas individuais de cada responsável
+  const somaResponsaveis = {};
 
   //Percorrer o Array de Lançamentos
   for (let i = 0; i < Lancamentos.length; i++) {
 
     const responsaveis = Lancamentos[i].Responsaveis;
     const PegaValor = Lancamentos[i].Valor;
-
+    //Calcula o valor individual a pagar por evento
     const ValorEvento = PegaValor / responsaveis.length;
-
-    var ValorPessoal = '';
 
     //Percorrer o Array de responsáveis
     for (let j = 0; j < responsaveis.length; j++) {
-      console.log(responsaveis[j]);
+      const responsavel = responsaveis[j];
 
-      textoHTML += `Responsáveis evento ${i+1}: ${responsaveis[j]}<br>`
+      // Incrementar o ValorEvento para o responsável atual
+      if (somaResponsaveis[responsavel]) {
+        somaResponsaveis[responsavel] += ValorEvento;
+      } else {
+        somaResponsaveis[responsavel] = ValorEvento;
+      }
 
-      //Adicionando o preço individual em cada usuário
-      Lancamentos[i].Responsaveis[j] = ValorEvento;
-
-      textoHTML2 += `Valor a pagar: ${Lancamentos[i].Responsaveis[j]}<br>`
-      console.log(ValorEvento)
     }
-
-    //Mostrar elementos na tela
-    var tela = document.getElementById('tela');
-    tela.innerHTML = textoHTML + textoHTML2;
-
   }
 
+  // Exibir a soma total para cada responsável
+  for (const responsavel in somaResponsaveis) {
+    console.log(`${responsavel}: ${somaResponsaveis[responsavel]}`);
+    var total = document.getElementById("tela");
+    total.innerHTML += `<div class="resultado"><p>${responsavel}</p><span>R$${somaResponsaveis[responsavel]}</span></div>`;
+  }
 }
